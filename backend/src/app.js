b115a54a8e5330b0362943df39867090623caf3c
Aadithya Senthil Kumaran <aadithyacps@gmail.com>
2026-01-20 22:19:30 +0530

    add railways install script and dual domain config file

--- backend/src/app.js
+++ backend/src/app.js
@@ -86,7 +86,7 @@ app.get("/:shortCode", async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
